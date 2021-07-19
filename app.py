b445b4e7487d75b00597786226c3a794819e76ea
Adam Burstyn <adam.burstyn@gmail.com>
2021-07-19 16:54:43 -0400

    added visualizations and clustering algorithm

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from pyvis import network as net
+from stvis import pv_static
+import clustering
 
 # Create interactive dashboard
 # Title
@@ -69,21 +72,47 @@ player_list = df["Player"].loc[df["Tm"]==input_data["team"][0]].sort_values().un
 st.subheader("User Inputs:")
 st.write(input_data)
 
+# Find the closest players
+pos = df["Pos"].loc[df["Player"]==input_data.player[0]].values[0]
+clustered_df = clustering.cluster(df.loc[df["Pos"]==pos], input_data.player[0])
+clustered_df = clustering.cluster(df.loc[(df["Pos"]==pos)&(df["Salary"]<=input_data.salary[0])], input_data.player[0])
+st.write(clustered_df)
+
 # Graph the player stats
 player_stats = df.loc[df["Player"]==input_data.player[0]]
 st.subheader("Player Stats:")
 st.write(player_stats)
-st.bar_chart(player_stats)
+
+#########Get closest player stats #######Need to update
+player2_stats = clustered_df.head(1)
+st.subheader("Player2 Stats:")
+st.write(player2_stats)
+
+# Network graph example
+g=net.Network(height='500px', width='500px',heading='')
+g.add_node(1)
+g.add_node(2)
+g.add_node(3)
+g.add_edge(1,2, value=0, weight=0, distance=500, springLength=200)
+g.add_edge(2,3, value=1, weight=.5, distance=100, springLength=1000)
+g.add_edge(1,3, value=.5, weight=1, distance=100, springLength=200)
+pv_static(g)
+
+# Network graph test
+g=net.Network(height='500px', width='500px',heading='')
+g.add_node(input_data.player[0])
+for index, row in clustered_df.iterrows():
+    g.add_node(row["Player"])
+    g.add_edge(input_data.player[0],row["Player"], value=5, weight=5)
+pv_static(g)
 
 #####Matplot example
 speed = [0.1, 17.5, 40, 48, 52, 69, 88]
 lifespan = [2, 8, 70, 1.5, 25, 12, 28]
 index = ['snail', 'pig', 'elephant',
          'rabbit', 'giraffe', 'coyote', 'horse']
-df_test = pd.DataFrame({'speed': speed,
-                   'lifespan': lifespan}, index=index)
+#df_test = pd.DataFrame({'speed': speed, 'lifespan': lifespan}, index=index)
 fig, ax = plt.subplots()
-#ax = df_test.plot.bar(rot=0)
 x = np.arange(len(index))
 width = 0.35
 ax.bar(x - width/2, lifespan, width, label="Lifespan")
@@ -92,16 +121,23 @@ ax.set_xticks(x)
 ax.set_xticklabels(index)
 ax.legend()
 st.pyplot(fig)
-#ax.bar(rot=0)
-#st.pyplot(fig)
-#st.bar_chart(df_test)
 
-#######Streamlit example
-# arr = np.random.normal(1, 1, size=100)
-# fig, ax = plt.subplots()
-# ax.hist(arr, bins=20)
-# st.pyplot(fig)
+#####Player graph test
+###########Scaling to keep certain columns
+player1 = player_stats.drop(columns=["Player", "Pos", "Tm", "MP", "Salary"])
+index = player1.columns
+player1 = player1.to_numpy()
 
-#################
+player2 = player2_stats.drop(columns=["Player", "Pos", "Tm", "MP", "Salary", "Label"])
+player2 = player2.to_numpy()
 
-
+fig, ax = plt.subplots()
+x = np.arange(len(index))
+width = 0.35
+ax.bar(x - width/2, player1[0].reshape(-1), width, label=input_data.player[0])
+ax.bar(x + width/2, player2[0].reshape(-1), width, label=player2_stats["Player"][0])
+ax.set_xticks(x)
+ax.set_xticklabels(index)
+plt.xticks(rotation = 60)
+ax.legend()
+st.pyplot(fig)
---
+++ clustering.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+
+def cluster(df1, player):
+    #df = dataframe
+
+    #df1 = df.loc[df["Pos"]==position]
+
+    # Create the input variables
+    X = df1.drop(columns=["Player", "Tm", "Pos", "ID", "Salary"])
+
+    clustering = AgglomerativeClustering(n_clusters=13).fit(X)
+    
+    df1["Label"] = clustering.labels_
+
+    player_label = df1.loc[df1["Player"]==player]["Label"].values[0]
+    # Return the dataframe of top 5 players similar to selected player
+    return df1.loc[(df1["Label"]==player_label)&(df1["Player"]!=player)].reset_index().drop(columns=["index"]).head()
+
+
+
+
+
